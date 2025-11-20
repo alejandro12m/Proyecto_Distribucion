c37a1a4087c5c35760736614d0ec1d6a71894634
alejandro12m <146794481+alejandro12m@users.noreply.github.com>
2025-11-20 10:50:48 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,50 +3,25 @@ using Microsoft.Extensions.DependencyInjection;
 using Distribucion.Core.Interfaces;
 using Distribucion.Infraestructura.Repositorio;
 using Distribucion.Infraestructura.Data;
+//DistribucionContext
 
-var builder = WebApplication.CreateBuilder(args);
+var url = Environment.GetEnvironmentVariable("DATABASE_URL");
+var builder WebApplication.CreateBuilder(args);
+builder. Services. AddDbContext<DistribucionContext>(options =>
+options. UseNpgsql(url));
+// Add services to the container.
+builder. Services. AddControllers();
 
-// Leer DATABASE_URL de Railway (ya viene como connection string)
-var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL");
-
-// Si no existe, intentar leer desde appsettings.json
-if (string.IsNullOrEmpty(connectionString))
-{
-    connectionString = builder.Configuration.GetConnectionString("DistribucionContext");
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder. Services.AddEndpointsApiExplorer();
+builder. Services.AddSwaggerGen();
+var app builder. Build();
+using (var scope app. Services.CreateScope())
+var db = scope. ServiceProvider. GetRequiredService<DistribucionContext>();
+db. Database. Migrate();
 }
-
-// Inyectar el contexto
-builder.Services.AddDbContext<DistribucionContext>(options =>
-    options.UseNpgsql(connectionString));
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("myApp", policibuilder =>
-    {
-        policibuilder.AllowAnyOrigin();
-        policibuilder.AllowAnyHeader();
-        policibuilder.AllowAnyMethod();
-    });
-});
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddHttpClient();
-
-builder.Services.AddScoped<IEnvioRepositorio, EnvioRepositorio>();
-builder.Services.AddScoped<IDetalleEnvioRepositorio, DetalleEnvioRepositorio>();
-
-var app = builder.Build();
-
-// Aplicar migraciones automáticamente al iniciar
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<DistribucionContext>();
-    db.Database.Migrate();
-}
-
-app.UseCors("myApp");
-app.UseAuthorization();
-app.MapControllers();
+app.UseSwagger();
+app. UseSwaggerUI();
+app. UseAuthorization();
+app. MapControllers();
 app.Run();
